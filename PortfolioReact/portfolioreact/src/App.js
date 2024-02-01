dd395433a2612b367a9e5e5e3f1f1496cbc2bdc6
yonx44 <sanderlawrence@gmail.com>
2024-02-01 10:40:54 +0800

    yes

--- PortfolioReact/portfolioreact/src/App.js
+++ PortfolioReact/portfolioreact/src/App.js
@@ -1,28 +1,23 @@
-import React from "react";
-import Banner from "./components/banner/Banner";
-import Contact from "./components/contact/Contact";
-import Features from "./components/features/Features";
-import Footer from "./components/footer/Footer";
-import FooterBottom from "./components/footer/FooterBottom";
-import Navbar from "./components/navbar/Navbar";
-import Projects from "./components/projects/Projects";
-import Resume from "./components/resume/Resume";
-import Testimonial from "./components/tesimonial/Testimonial";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div className="w-full h-auto bg-bodyColor text-lightText px-4">
-        <Navbar />
-      <div className="max-w-screen-xl mx-auto">
-        <Banner />
-        <Features />
-        <Projects />
-        <Resume />
-        <Testimonial />
-        <Contact />
-        <Footer />
-        <FooterBottom />
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
